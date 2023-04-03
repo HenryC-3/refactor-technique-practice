d9b2cefbd7b08ca060c0d202d51e956d901788ad
HenryC-3 <2324375439@qq.com>
2023-04-03 18:00:11 +0800

    split phase: init

---
+++ src/split phase/index.ts
@@ -0,0 +1,32 @@
+interface Product {
+	basePrice: number;
+	discountThreshold: number;
+	discountRate: number;
+}
+
+interface ShippingMethod {
+	discountThreshold: number;
+	discountedFee: number;
+	feePerCase: number;
+}
+function priceOrder(
+	product: Product,
+	quantity: number,
+	shippingMethod: ShippingMethod
+) {
+	// 阶段 1
+	const basePrice = product.basePrice * quantity;
+	const discount =
+		Math.max(quantity - product.discountThreshold, 0) *
+		product.basePrice *
+		product.discountRate;
+
+	// 阶段 2
+	const shippingPerCase =
+		basePrice > shippingMethod.discountThreshold
+			? shippingMethod.discountedFee
+			: shippingMethod.feePerCase;
+	const shippingCost = quantity * shippingPerCase;
+	const price = basePrice - discount + shippingCost;
+	return price;
+}
